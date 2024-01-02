2b08b6535ba008d0cdc9870c31681ef246b2a64e
krzysztofmotas <127015975+krzysztofmotas@users.noreply.github.com>
2024-01-02 17:21:57 +0100

    rozpoczęcie prac nad projektem

---
+++ src/LoginForm.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class LoginForm extends JFrame {
+    public LoginForm() {
+        setTitle("Elektroniczny system oceniania");
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+}
